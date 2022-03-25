edb6e845b09973967812d007b513d91150313126
delvinalfredo <delvinalfredo@gmail.com>
2022-03-25 11:57:23 +0700

    Website Intermite

---
+++ ext.js
@@ -0,0 +1,11 @@
+function service(){
+    alert("This service isn't available yet")
+}
+
+function changeText(id){
+    id.innerHTML = "INTERMITE"
+}
+
+function undoText(id){
+    id.innerHTML = "intermite"
+}
